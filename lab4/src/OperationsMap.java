df2c3280083e63e3d571f6f6f407ef47efb28924
jonathan <jonathanramirez4479@gmail.com>
2023-05-14 20:30:44 -0700

    - refactored the OperationsMap because our arithmetic operations had errors if a register value was NULL (Integer 0 is null), so I changed the code as necessary for the 0 values to be of type int
    - added showPipe(): shows the pipeline registers
    
    - added instrToPipe(): takes in a new instruction string as an argument and checks if any delays should be applied
    
    - added normalInsert(): does a normal insertion of the new string instruction to the pipeline and moves the other instructions as appropriate
    
    - initialized a new simulator counter which is different from the program count
    
    - initialized a pipe register array to hold our string instructions in the pipeline

--- lab4/src/OperationsMap.java
+++ lab4/src/OperationsMap.java
@@ -46,18 +46,18 @@ public class OperationsMap {
     public static void ADD(Instructions instr){
         // add rd, rs, rt
         // rd = rs + rt
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(2));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
 
         Integer rd = rs + rt;
-        // replace old rt with new rt
+        // replace old rd with new rd
         RegisterFile.RF.replace(instr.operands.get(0), rd);
     }
     public static void AND(Instructions instr){
         // and rd, rs, rt
         // rd = rs & rt
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(2));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
         Integer rd = rs & rt;
         // replace old rt with new rt
         RegisterFile.RF.replace(instr.operands.get(0), rd);
@@ -65,8 +65,8 @@ public class OperationsMap {
     public static void OR(Instructions instr){
         // or rd, rs, rt
         // rd = rs | rt
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(2));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
         Integer rd = rs | rt;
         // replace old rt with new rt
         RegisterFile.RF.replace(instr.operands.get(0), rd);
@@ -74,16 +74,16 @@ public class OperationsMap {
     public static void SLL(Instructions instr){
         // sll rd, rt, sa
         // rd = rt << sa
-        Integer rt = RegisterFile.RF.get(instr.operands.get(1));
-        Integer sa = Integer.parseInt(instr.operands.get(2)); //This will be a constant
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int sa = checkNullInteger(Integer.parseInt(instr.operands.get(2))); //This will be a constant
         Integer rd = rt << sa;
         RegisterFile.RF.replace(instr.operands.get(0), rd);
     }
     public static void SUB(Instructions instr){
         // sub rd, rs, rt
         // rd = rs - rt
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(2));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
         Integer rd = rs - rt;
         // replace old rt with new rt
         RegisterFile.RF.replace(instr.operands.get(0), rd);
@@ -93,9 +93,9 @@ public class OperationsMap {
         // slt rd, rs, rt
         // if rs < rt: rd = 1
         // else rd = 0
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(2));
-        Integer rd;
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
+        int rd;
         if(rs < rt)
             rd = 1;
         else
@@ -106,13 +106,14 @@ public class OperationsMap {
         // gets content of register (operand[0]) and sets it to PC
         // jr rs
         // pc = rs
-        lab4.counter = RegisterFile.RF.get(instr.operands.get(0))-1;
+        int new_counter = checkNullInteger(RegisterFile.RF.get(instr.operands.get(0)))-1;
+        lab4.counter = new_counter;
     }
     public static void ADDI(Instructions instr){
         // addi rt, rs, imm
         // rt = rs + imm
-        Integer rs = RegisterFile.RF.get(instr.operands.get(1));
-        Integer imm = Integer.parseInt(instr.operands.get(2));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int imm = checkNullInteger(Integer.parseInt(instr.operands.get(2)));
         // reassign rt with sum
         Integer rt = rs + imm;
         RegisterFile.RF.replace(instr.operands.get(0), rt); // replace reg value with new rt
@@ -120,48 +121,57 @@ public class OperationsMap {
     public  static void BEQ(Instructions instr){
         // bne rs, rt, offset
         // if(rt == rs) go to offset
-        Integer rs = RegisterFile.RF.get(instr.operands.get(0));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(1));
-        Integer offset = readFile.labels.get(instr.operands.get(2));
-        if(!rt.equals(rs)){
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(0)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int offset = checkNullInteger(readFile.labels.get(instr.operands.get(2)));
+        if(rt == rs){
             lab4.counter = offset-1; // set program counter to line of label (account for offset of next iteration)
         }
     }
     public static void BNE(Instructions instr){
         // bne rs, rt, offset
         // if(rt != rs) go to offset
-        Integer rs = RegisterFile.RF.get(instr.operands.get(0));
-        Integer rt = RegisterFile.RF.get(instr.operands.get(1));
-        Integer offset = readFile.labels.get(instr.operands.get(2));
-        if(!rt.equals(rs)){
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(0)));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(1)));
+        int offset = checkNullInteger(readFile.labels.get(instr.operands.get(2)));
+        if(rt != rs){
             lab4.counter = offset-1; // set program counter to line of label (account for offset of next iteration)
         }
     }
     public static void LW(Instructions instr){
         // lw rt, imm(rs)
         // rt = memory[immm + rs]
-        Integer imm = Integer.parseInt(instr.operands.get(1));
-        Integer rs = RegisterFile.RF.get(instr.operands.get(2));
+        int imm = checkNullInteger(Integer.parseInt(instr.operands.get(1)));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
         Integer rt = lab4.memory[imm + rs]; // access memory
         RegisterFile.RF.replace(instr.operands.get(0), rt); // store new rt reg value
     }
     public static void SW(Instructions instr){
         // sw rt, imm(rs)
         // memory[imm + rs] = rt
-        Integer rt = RegisterFile.RF.get(instr.operands.get(0));
-        Integer imm = Integer.parseInt(instr.operands.get(1));
-        Integer rs = RegisterFile.RF.get(instr.operands.get(2));
+        int rt = checkNullInteger(RegisterFile.RF.get(instr.operands.get(0)));
+        int imm = checkNullInteger(Integer.parseInt(instr.operands.get(1)));
+        int rs = checkNullInteger(RegisterFile.RF.get(instr.operands.get(2)));
         lab4.memory[imm + rs] = rt; // store rt in memory
     }
     public static void J(Instructions instr){
         // Sets PC to corresponding label
-        lab4.counter = readFile.labels.get(instr.operands.get(0));
+        int new_counter = checkNullInteger(readFile.labels.get(instr.operands.get(0)));
+        lab4.counter = new_counter;
     }
     public static void JAL(Instructions instr){
         // Sets $ra to current count, then replaces PC to label
         // jal target
-        int newLocation = readFile.labels.get(instr.operands.get(0)); // get jump location
-        RegisterFile.RF.replace("$ra", lab4.counter+1); // set $ra to current line
+        int newLocation = checkNullInteger(readFile.labels.get(instr.operands.get(0))); // get jump location
+        RegisterFile.RF.replace("$ra", lab4.counter+1); // set $ra to next instr
         lab4.counter = newLocation; //set new location
+    }
+
+    public static int checkNullInteger(Integer value)
+    {
+        if(value == null)
+            return 0;
+        else
+            return value;
     }
 }
--- lab4/src/lab4.java
+++ lab4/src/lab4.java
@@ -12,8 +12,15 @@ import java.util.*;
 
 public class lab4 {
     public static int[] memory = new int[8192]; // data memory
+    public static String[] pipeReg = new String[4]; // pipeline registers
     public static int counter = 0; // program counter
+    public static int simCounter = 0; // cpu simulator counter
+    public static int cycleCount = 0; // cycle count for the program
     public static void main(String[] args) throws IOException {
+        pipeReg[0] = "empty";
+        pipeReg[1] = "empty";
+        pipeReg[2] = "empty";
+        pipeReg[3] = "empty";
         readFile.readFile_main(args[0]);
 
         //Determine Mode
@@ -45,7 +52,6 @@ public class lab4 {
 
 
         RegisterFile.initRF(); // init register file
-        int instrMemSize = readFile.instructionsList.size();
 
         while (counter <= readFile.instructionsList.size()){
             System.out.print("mips> ");
@@ -53,18 +59,17 @@ public class lab4 {
             String[] command = reader.readLine().trim().split(" ");
             lab3Functions(command);
         }
-
     }
-
     private static void lab3Functions(String[] params){
         if(params[0].equalsIgnoreCase("h")){
             // show help
             System.out.println(
-                    "\nh = show help\n" +
+                            "\nh = show help\n" +
                             "d = dump register state\n" +
-                            "s = single step through the program (i.e. execute 1 instruction and stop)\n" +
-                            "s num = step through num instructions of the program\n" +
-                            "r = run until the program ends\n" +
+                            "p = show pipeline registers\n" +
+                            "s = single step through the program (i.e. simulate 1 cycle and stop)\n" +
+                            "s num = step through num clock cycles\n" +
+                            "r = run until the program ends and display the timing summary\n" +
                             "m num1 num2 = display data memory from location num1 to num2\n" +
                             "c = clear all registers, memory, and the program counter to 0\n" +
                             "q = exit the program\n");
@@ -86,7 +91,8 @@ public class lab4 {
             if(params.length == 1) {
                 Instructions currentInstr = readFile.instructionsList.get(counter);
                 OperationsMap.findOp(currentInstr);
-                System.out.println("    1 instruction(s) executed ");
+                instrToPipe(currentInstr.instruction);
+                showPipe();
                 if(!currentInstr.getType().equals("J")) {
                     counter++;
                 }
@@ -100,21 +106,25 @@ public class lab4 {
                     }
                     i--;
                 }
-                System.out.println("    " + params[1] +  " instruction(s) executed ");
+
+                showPipe();
             }
         }
         else if(params[0].trim().equalsIgnoreCase("r")){
             // run until program stops
-            while(counter < readFile.instructionsList.size()){
+            while(counter < readFile.instructionsList.size())
+            {
                 Instructions currentInstr = readFile.instructionsList.get(counter);
                 OperationsMap.findOp(currentInstr);
-                if(!currentInstr.getType().equals("J")) {
+                instrToPipe(currentInstr.instruction);
+                if(!currentInstr.getType().equals("J"))
+                {
                     counter++;
                 }
-                if(counter == readFile.instructionsList.size()){
-                    break;
-                }
             }
+            // program finished - add the remaining instructions (in the pipe) to the cycle count
+            cycleCount += pipeReg.length;
+            System.out.println("cycle count: " + cycleCount);
         }
         else if (params[0].trim().equalsIgnoreCase("c")){
             // clear registers, memory and set counter to 0
@@ -136,7 +146,70 @@ public class lab4 {
             }
             System.out.println();
         }
-
     }
 
+    public static void showPipe()
+    {
+            System.out.println("pc      if/id   id/exe  exe/mem mem/wb");
+            System.out.println(simCounter + "      " + pipeReg[0] + "   " + pipeReg[1] + "    " +
+                    pipeReg[2] + "   " + pipeReg[3]);
+    }
+    public static void instrToPipe(String newInstr)
+    {
+        // newInstr is the instruction waiting at the pc
+        /*
+        pipeReg[0] -> if/id
+        pipeReg[1] -> id/exe
+        pipeReg[2] -> exe/mem
+        pipeReg[3] -> mem/wb
+        1) use-after-load
+        Notes:
+        lw rt, imm(rs)
+            a. check if lw is in register pipeline id/exe
+            b. create Instruction var's for current and previous instruction
+                i.e:  if/id and id/exe
+            c. create strings for register names and compare
+            d. check if current instr. has reg's that use rd from previous lw
+                d1. if so, then stall; else, normal cycle
+         */
+        if(pipeReg[1].equals("lw")) // check if id/exe has lw
+        {
+            Instructions curr_instr = readFile.instructionsList.get(simCounter-1); // current instr (after load)
+            Instructions lw_instr = readFile.instructionsList.get(simCounter-2); // prev instr (lw)
+            // NOTE: will only support: add, and, or, sub, slt (because these are the ones being tested)
+            // op rd rs rt
+            String lw_rt = lw_instr.operands.get(0);
+            String instr_rs = curr_instr.operands.get(1);
+            String instr_rt = curr_instr.operands.get(2);
+
+            if(lw_rt.equalsIgnoreCase(instr_rs) || lw_rt.equalsIgnoreCase(instr_rt))
+            {
+                // STALL
+                pipeReg[3] = pipeReg[2];
+                pipeReg[2] = lw_instr.instruction;
+                pipeReg[1] = "stall";
+                counter = simCounter-1;
+                cycleCount++;
+            }
+            else
+                normalInsert(newInstr);
+        }
+        else
+            normalInsert(newInstr);
+    }
+    public static void normalInsert(String newInstr)
+    {
+        String[] firstThree = new String[pipeReg.length - 1];
+        firstThree[0] = pipeReg[0];
+        firstThree[1] = pipeReg[1];
+        firstThree[2] = pipeReg[2];
+
+        pipeReg[0] = newInstr;
+        pipeReg[1] = firstThree[0];
+        pipeReg[2] = firstThree[1];
+        pipeReg[3] = firstThree[2];
+
+        simCounter++;
+        cycleCount++;
+    }
 }
