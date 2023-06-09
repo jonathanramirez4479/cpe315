3d995a4c1ede420ea8bd62a4c1327a81b6ee18f3
jonathan <jonathanramirez4479@gmail.com>
2023-06-09 08:59:38 -0700

    added our names, ready for submission

--- lab6/src/lab6.java
+++ lab6/src/lab6.java
@@ -1,4 +1,8 @@
-
+/*
+Authors: Franco Abullarade, Jonathan Ramirez
+Project: Lab 6
+Class: CPE 315-03
+*/
 public class lab6 {
     public static void main(String[] args) {
         Caches.InitCaches();
