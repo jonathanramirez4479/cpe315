d0002e83463742e41fdc66cce75bb4f2c5f567e6
jonathan <jonathanramirez4479@gmail.com>
2023-06-09 08:37:36 -0700

    added makefile and last test case

--- lab6/src/lab6.java
+++ lab6/src/lab6.java
@@ -2,7 +2,7 @@
 public class lab6 {
     public static void main(String[] args) {
         Caches.InitCaches();
-        readFile.parseFile("src/mem_stream.1");
+        readFile.parseFile(args[0]);
         Caches.printCaches();
     }
 }
